05f78964bf2bea08699173a2e6c8977cbc104bec
ben-aaron188 <bennettkleinberg@BAR-MacBook-Air.local>
2015-05-29 15:17:26 +0100

    prepared funcs

---
+++ main.js
@@ -0,0 +1,27 @@
+function rearrange_fields(input){
+
+}
+
+function choose_field(input){
+
+}
+
+function set_timer(dur){
+
+}
+
+function add_miss(){
+
+}
+
+function get_server_matrix(){
+
+}
+
+function decide_start(){
+
+}
+
+function send_server_matrix(){
+
+}
