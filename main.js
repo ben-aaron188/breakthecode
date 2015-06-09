9e521c0f6d7bdbe82d1a07b89f32bda38cdae082
Maximilian Mozes <maximilian@mozes.de>
2015-06-09 11:27:43 +0200

    1st

---
+++ main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+	css_center($("#code_notice_top"));
+	css_center($("#code_notice_bottom"));
+	//clickdiv($("#grid_top1"));
+});
+
+/*
+function clickdiv(object) {
+	$(object).on("click", function() {
+			$(this).css("background-color" ,"white");
+	});
+}
+*/
